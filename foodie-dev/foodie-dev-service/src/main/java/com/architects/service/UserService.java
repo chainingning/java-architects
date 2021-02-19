b63c212259f66bcbc14b5ddb8be74136d1424d1f
chaining <ning.chai@foxmail.com>
2021-02-19 17:03:34 +0800

    添加依赖

---
+++ foodie-dev/foodie-dev-service/src/main/java/com/architects/service/UserService.java
@@ -0,0 +1,14 @@
+package com.architects.service;
+
+/**
+ * @ClassName UserService
+ * @Description:
+ * @Author ning.chai@foxmail.com
+ * @Date 2021/2/19 0019
+ * @Version V1.0
+ **/
+public interface UserService {
+
+    boolean queryUsername(String username);
+
+}
---
+++ foodie-dev/foodie-dev-service/src/main/java/com/architects/service/impl/userServiceImpl.java
@@ -0,0 +1,18 @@
+package com.architects.service.impl;
+
+import com.architects.service.UserService;
+
+/**
+ * @ClassName userServiceImpl
+ * @Description:
+ * @Author ning.chai@foxmail.com
+ * @Date 2021/2/19 0019
+ * @Version V1.0
+ **/
+public class userServiceImpl implements UserService {
+
+    @Override
+    public boolean queryUsername(String username) {
+        return false;
+    }
+}
