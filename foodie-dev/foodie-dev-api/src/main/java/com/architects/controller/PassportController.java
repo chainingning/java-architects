fc4577dbe325aa01c4c75ea3a4cc1f6733a8a583
chaining <ning.chai@foxmail.com>
2021-02-20 17:13:14 +0800

    优化swagger2显示

--- foodie-dev/foodie-dev-api/src/main/java/com/architects/controller/PassportController.java
+++ foodie-dev/foodie-dev-api/src/main/java/com/architects/controller/PassportController.java
@@ -3,6 +3,8 @@ package com.architects.controller;
 import com.architects.bo.UserBO;
 import com.architects.service.UserService;
 import com.architects.utils.JSONVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,7 @@ import javax.validation.Valid;
  * @Date 2021/2/19 0019
  * @Version V1.0
  **/
+@Api(value = "注册登录",tags = {"用于注册登录的相关接口"})
 @RestController
 @RequestMapping("passport")
 public class PassportController {
@@ -23,7 +26,7 @@ public class PassportController {
     @Autowired
     private UserService userService;
 
-
+    @ApiOperation(value = "判断用户是否存在",notes ="判断用户是否存在",httpMethod = "GET")
     @GetMapping("/usernameIsExist")
     public JSONVO usernameIsExist(@RequestParam String username){
 
@@ -40,6 +43,8 @@ public class PassportController {
         return JSONVO.ok();
     }
 
+
+    @ApiOperation(value = "用户注册",notes ="用户注册",httpMethod = "POST")
     @PostMapping("/register")
     public JSONVO register(@Valid @RequestBody UserBO userBO){
         userService.createUser(userBO);
--- foodie-dev/foodie-dev-pojo/src/main/java/com/architects/bo/UserBO.java
+++ foodie-dev/foodie-dev-pojo/src/main/java/com/architects/bo/UserBO.java
@@ -1,5 +1,7 @@
 package com.architects.bo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
@@ -13,15 +15,14 @@ import javax.validation.constraints.NotNull;
  * @Date 2021/2/20 0020
  * @Version V1.0
  **/
-
+@ApiModel(value = "用户对象BO",description = "从客户端，由用户传入的数据封装在此entity中")
 public class UserBO {
 
+    @ApiModelProperty(value = "用户名",name = "username",example = "chaining",required = true)
     @NotNull(message = "用户名不能为空")
     private String username;
-
     @NotNull(message = "密码不能为空")
     private String password;
-
     private String confirmPassword;
 
     public String getUsername() {
