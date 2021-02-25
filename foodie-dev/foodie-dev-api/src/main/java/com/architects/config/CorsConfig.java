02395939858969de6fefaade30ca7e150e09f021
chaining <ning.chai@foxmail.com>
2021-02-25 17:06:47 +0800

    跨域配置

--- foodie-dev/foodie-dev-api/src/main/java/com/architects/config/CorsConfig.java
+++ foodie-dev/foodie-dev-api/src/main/java/com/architects/config/CorsConfig.java
@@ -1,8 +1,10 @@
 package com.architects.config;
 
 import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 /**
@@ -16,10 +18,22 @@ import org.springframework.web.filter.CorsFilter;
 @NoArgsConstructor
 public class CorsConfig {
 
+    @Bean
     public CorsFilter corsFilter(){
         CorsConfiguration corsConfiguration = new CorsConfiguration();
+
         corsConfiguration.addAllowedMethod("*");
-//        corsConfiguration.ad
-        return null;
+
+        corsConfiguration.addAllowedOrigin("http://localhost:8080");
+
+        corsConfiguration.addAllowedHeader("*");
+
+        //设置是否发送cookie信息
+        corsConfiguration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource corsSource = new UrlBasedCorsConfigurationSource();
+        corsSource.registerCorsConfiguration("/**",corsConfiguration);
+
+        return new CorsFilter(corsSource);
     }
 }
