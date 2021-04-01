7f73b4a8587927e9f9f231909156c6353c3a49fe
chaining <ning.chai@foxmail.com>
2021-04-01 11:03:25 +0800

    数据库脚本

---
+++ foodie-dev/sql/20201126.sql
@@ -0,0 +1,44 @@
+select o.id as orderId,
+       o.created_time as createdTime,
+       o.pay_method as payMethod,
+       o.real_pay_amount as payMethod,
+       o.post_amount as postAmount,
+       os.order_status as orderStatus,
+       oi.item_id as itemId,
+       oi.item_name as itemName,
+       oi.item_img as itemImg,
+       oi.item_spec_id as itemSpecId,
+       oi.item_spec_name as itemSpecName,
+       oi.buy_counts as buyCounts,
+       oi.price as price
+from order o
+left join order_status os
+on o.id = os.order_id
+left join order_items oi
+on o.id = oi.order_id
+where o.user_id = '1908189H7TNWDTXP'
+and o.is_delete = 0
+order by o.updated_time;
+
+update order_status
+set order_status = 30,
+    deliver_time = ''
+where order_id = '';
+
+select *
+from order
+where user_id = ''
+and id = ''
+and is_delete = 0;
+
+update order_status
+set order_status = 40,
+    success_time = ''
+where order_id = ''
+and order_status = 30;
+
+update order
+set is_delete = 1,
+    updated_time = ''
+where id = ''
+  and user_id = '';
---
+++ foodie-dev/sql/20201127.sql
@@ -0,0 +1,41 @@
+select * from order_items where order_id = '';
+
+select ic.id as commentId,
+       ic.content as content,
+       ic.created_time as createdTime,
+       ic.item_id as itemId,
+       ic.item_name as itemName,
+       ic.sepc_name as sepcName,
+       ii.url as itemImg
+from items_comments ic
+left join items_img ii
+on ic.item_id = ii.item_id
+where ic.user_id = ''
+and ii.is_main = 1
+order by ic.created_time
+desc;
+
+select count(*)
+from order o
+left join order_status os
+on o.id = os.order_id
+where o.user_id = ''
+and os.order_status = 10;
+# and o.is_comment = 0;
+
+select os.order_id as orderId,
+       os.order_status as orderStatus,
+       os.created_time as createdTime,
+       os.pay_time as payTime,
+       os.deliver_time as deliverTime,
+       os.success_time as sucessTime,
+       os.close_time as closeTime,
+       os.comment_time as commentTime
+from order o
+left join order_status os
+on o.id = os.order_id
+where o.is_delete = 0
+and o.user_id = ''
+and os.order_status in (20, 30, 40)
+order by os.order_id
+desc;
